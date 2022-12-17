5f4640f38db0b0ea6b5721c6ddc6a6683c5879da
proffapt <proffapt@pm.me>
2022-12-17 18:25:26 +0530

    v1.1.7

--- code-runner.rb
+++ code-runner.rb
@@ -1,8 +1,8 @@
 class CodeRunner < Formula
   desc "All-in-one utility to execute code files"
   homepage "https://github.com/proffapt/code-runner"
-  url "https://github.com/proffapt/code-runner/archive/refs/tags/v1.1.6.tar.gz"
-  sha256 "e42aa0003eb681f76d0565772cc3ad9a68454381c42f96ac2c86975360dfa85c"
+  url "https://github.com/proffapt/code-runner/archive/refs/tags/v1.1.7.tar.gz"
+  sha256 "36da307fc1087b3aef82dad99207ae2a8cd5fd6ad34ee24df6aa87db20285893"
   license "BSD-2-Clause"
 
   def install
