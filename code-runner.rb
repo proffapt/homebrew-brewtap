cbf429bb7202416cca457c73d5203d464fcca2ea
proffapt <proffapt@pm.me>
2023-04-26 14:51:48 +0530

    caveats: how to setup keybinding docs

--- code-runner.rb
+++ code-runner.rb
@@ -1,5 +1,5 @@
 class CodeRunner < Formula
-  desc "All-in-one utility to execute code files"
+  desc "All-in-one command-line utility to execute code files"
   homepage "https://github.com/proffapt/code-runner"
   url "https://github.com/proffapt/code-runner/archive/refs/tags/v1.1.7.tar.gz"
   sha256 "36da307fc1087b3aef82dad99207ae2a8cd5fd6ad34ee24df6aa87db20285893"
@@ -8,4 +8,14 @@ class CodeRunner < Formula
   def install
     bin.install "run"
   end
+
+  def caveats
+    <<~EOS
+      To configure keybindings for code-runner on your preferred IDE(s), refer to the following links:
+      
+        VS Code : https://github.com/proffapt/code-runner/tree/main#integrating-with-vscode
+        (Neo)VIM : https://github.com/proffapt/code-runner/tree/main#integrating-with-vim
+    EOS
+  end
+
 end
