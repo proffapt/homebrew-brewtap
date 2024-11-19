25884bedd8946dca116dda25970bdb3a82e3a1cf
proffapt <proffapt@pm.me>
2024-11-19 15:17:18 +0000

    add-release(rewrk): v0.1.0

--- rewrk.rb
+++ rewrk.rb
@@ -1,8 +1,8 @@
 class ReWRK < Formula
   desc "Reworked wrk - Managed HTTP benchmarking tool"
   homepage "https://github.com/proffapt/rewrk"
-  url "https://github.com/proffapt/rewrk/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "666"
+  url "https://github.com/proffapt/rewrk/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "0ab2b9ab05fbb98bd714aa460e09ce75cf79c31b817e53fab537a739cd3dbd0a"
   license "AGPL-3.0 license"
 
   depends_on "wrk"
